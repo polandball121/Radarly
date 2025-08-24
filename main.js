5a15d649742445d83c5e18e1372b687fff55163e
polandball121 <146040317+polandball121@users.noreply.github.com>
2025-08-24 10:40:23 -1000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-const CLIENT_ID = '1407901384187183284';
-const REDIRECT_URI = encodeURIComponent(window.location.origin + '/callback');
-const SCOPE = 'identify';
-const actionBtn = document.getElementById('action-btn');
+// This version did NOT check login persistently
+// Random background for home page
 
 const images = [
   '/images/bg1.PNG',
@@ -16,28 +14,8 @@ const images = [
   '/images/bg10.PNG'
 ];
 
-// Random background image
 const randomImage = images[Math.floor(Math.random() * images.length)];
 document.body.style.backgroundImage = `url('${randomImage}')`;
-
-// Check if user is already logged in
-async function checkLogin() {
-  try {
-    const res = await fetch('/check-login');
-    const data = await res.json();
-
-    if (data.loggedIn) {
-      actionBtn.textContent = `Continue as ${data.username}`;
-      actionBtn.href = '/'; // Continue to homepage
-    } else {
-      actionBtn.textContent = 'Login with Discord';
-      actionBtn.href = `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`;
-    }
-  } catch (err) {
-    console.error('Error checking login:', err);
-    actionBtn.textContent = 'Login with Discord';
-    actionBtn.href = `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`;
-  }
-}
-
-checkLogin();
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundPosition = 'center';
+document.body.style.backgroundRepeat = 'no-repeat';
